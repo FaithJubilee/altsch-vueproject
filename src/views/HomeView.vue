c17f267fd0eaf64fd7849d1018ff4cb4f490123c
FaithJubilee <jubileefaith36@gmail.com>
2023-03-18 21:23:53 +0100

    displaying datas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome Faith Jubilee</h1>
+
+    <div class="descriptions">
+      <div :key="users.id">
+        <img v-bind:src="users.avatar_url" alt="mapic" class="ma-pic" />
+        <p class="name">{{ users.name }}</p>
+        <p>{{ users.login }}</p>
+        <p class="bio">{{ users.bio }}</p>
+      </div>
+      <div class="get-repos">
+        <router-link to="/repos">Get Repos</router-link>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+   data() {
+    return {
+      users: {},
+    };
+  },
+
+   mounted() {
+     axios.get("https://api.github.com/users/FaithJubilee").then((resp) => {
+      this.users = resp.data;
+      console.warn(this.users);
+    });
+  },
+};
 </script>
---
+++ src/views/RepoView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="repos">
+    <h3>Repositories</h3>
+    <div v-for="repo in users" :key="repo.id" class="repos">
+      <p>{{ repo.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+
+  mounted() {
+    axios.get(`https://api.github.com/users/jahseed89/repos`).then((resp) => {
+      this.users = resp.data;
+      console.warn(this.users);
+    });
+  },
+};
+</script>
